7385ae8d97ba93b2860ec80d4521022b83516777
Tanesh Sharma <taneshsharmaofficial@gmail.com>
2025-03-18 20:32:56 +0530

    Projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Projects = () => (
+    <section id="projects" className="projects">
+        <h2>Work & Projects</h2>
+    </section>
+);
+
+export default Projects;
